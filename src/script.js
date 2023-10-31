fdf4a4e05c3ed21f146b491e1eec5df1295442ee
Rishi Raj <rishiraj007bgp@gmail.com>
2023-10-31 14:54:12 +0530

    add image to webpage

--- src/script.js
+++ src/script.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import myIcon from './man.png';
+
 
 function component() {
     const element = document.createElement('div')
     element.textContent = _.join(['Hello', 'Webpack'], ' ');
     element.classList.add('hello');
+
+    const icon = new Image()
+    icon.src = myIcon;
+
+    element.appendChild(icon)
 
     return element;
 }
